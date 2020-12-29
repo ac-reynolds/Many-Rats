f4315a7632f16cb372f1c54512c647892216ec24
ac-reynolds <adam.chiu.reynolds@gmail.com>
2020-12-29 03:44:13 -0800

    disabled personbehaviour, gamemanager, and rathordebehaviour scripts
    scripts were throwing bugs, removed them from prefabs until we can fix that

---
+++ Many Rats/Assets/Scripts/Pathing/WalkableNode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Node class for specifying walkable graphs.  
+ */
+public class WalkableNode : MonoBehaviour
+{
+
+    public List<WalkableNode> neighbors;
+    private List<float> _nodeDistances;
+
+    void Awake () {
+        //update neighbor list of other nodes, for consistency
+        foreach (WalkableNode n in neighbors) {
+            if (!n.neighbors.Contains(this)) {
+                Debug.Log("not in nieghbor :(");
+            }
+        }
+    }
+
+    void Start()
+    {
+        //initialize graph with distances
+        _nodeDistances = new List<float>();
+
+        for (int i = 0; i < neighbors.Count; i++) {
+            float distToNeighbor = Vector2.Distance(transform.position, neighbors[i].transform.position);
+            _nodeDistances.Add(distToNeighbor);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Many-Rats
 Game for LM0B Beginners Game Jam #8
 Using Unity version: 2020.1.0f1
+
+Assets taken from:
+polyperfect 3d object packs
+https://www.reddit.com/r/DnD/comments/7nf4gl/art_hammerfel_the_town_map/
